393b6718df53b83670c7b33f64ca17f2f7077ef0
EugeneL01 <eugene@webwiser.io>
2024-01-24 17:03:59 +0200

    update

--- src/app/components/CurrentDrivers/index.tsx
+++ src/app/components/CurrentDrivers/index.tsx
@@ -1,20 +1,28 @@
 import { getDriverData } from "@/store/api/driver-data";
+import './styles.css'
 
 const CurrentDrivers = async () => {
   const data = await getDriverData();
   return (
-    <div>
+    <div className="flex flex-col gap-[30px]">
       {data.map((data: any, index: any) => (
-        <div key={index} className="pl-[20px]">
-          <p>{data.full_name}</p>
-          <p>{data.country_code}</p>
-          <p>{data.driver_number}</p>
-          <div
-            className="w-[3px] h-[10px]"
+        <div key={index} className="pl-[20px] flex flex-row">
+          {/* <div
+            className="w-[4px] h-[30px]"
             style={{ backgroundColor: `#${data.team_colour}` }}
-          ></div>
-          <p>{data.team_name}</p>
-          <img src={data.headshot_url} alt="" />
+          ></div> */}
+          <div className="inner-container relative pl-[10px]" style={{ color: `#${data.team_colour}` }}>
+            <div className="text-black">
+              <div className="flex flex-col">
+                <p>{data.first_name}</p>
+                <p>{data.last_name}</p>
+              </div>
+              <p>{data.country_code}</p>
+              <p>{data.team_name}</p>
+              <img src={data.headshot_url} alt="" />
+              <p>{data.driver_number}</p>
+            </div>
+          </div>
         </div>
       ))}
     </div>
