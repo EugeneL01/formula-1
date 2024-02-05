a99170a884241ba3b26af7f1bdc230e235420370
EugeneL01 <eugene@webwiser.io>
2024-02-05 11:57:36 +0200

    update

--- src/app/components/CurrentDrivers/index.tsx
+++ src/app/components/CurrentDrivers/index.tsx
@@ -1,30 +1,35 @@
 import { getDriverData } from "@/store/api/driver-data";
-import './styles.css'
+import "./styles.css";
 
 const CurrentDrivers = async () => {
   const data = await getDriverData();
   return (
-    <div className="flex flex-col gap-[30px]">
-      {data.map((data: any, index: any) => (
-        <div key={index} className="pl-[20px] flex flex-row">
-          {/* <div
-            className="w-[4px] h-[30px]"
-            style={{ backgroundColor: `#${data.team_colour}` }}
-          ></div> */}
-          <div className="inner-container relative pl-[10px]" style={{ color: `#${data.team_colour}` }}>
-            <div className="text-black">
-              <div className="flex flex-col">
-                <p>{data.first_name}</p>
-                <p>{data.last_name}</p>
+    <div className="max-w-[1500px] mx-auto md:px-[30px] px-[16px] py-[80px] w-full">
+      <div className="flex justify-center">
+        <div className="grid min-[550px]:grid-cols-[repeat(auto-fill,_minmax(350px,_1fr))] justify-items-stretch gap-[30px] w-full">
+          {data.map((data: any, index: any) => (
+            <div
+              key={index}
+              className="pl-[20px] flex flex-row p-[20px] border border-gray-500 rounded-[5px]"
+            >
+              <div
+                className="inner-container relative pl-[10px]"
+                style={{ color: `#${data.team_colour}` }}
+              >
+                <div className="text-black">
+                  <div className="flex flex-row justify-between">
+                    <p>{data.full_name}</p>
+                  </div>
+                  <p>{data.country_code}</p>
+                  <img src={data.headshot_url} alt="" />
+                  <p>{data.driver_number}</p>
+                  <p>{data.team_name}</p>
+                </div>
               </div>
-              <p>{data.country_code}</p>
-              <p>{data.team_name}</p>
-              <img src={data.headshot_url} alt="" />
-              <p>{data.driver_number}</p>
             </div>
-          </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
